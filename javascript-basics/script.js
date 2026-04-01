ef9b19b8c1853603fc34f663f9ce2aea3824aa4a
Amanda <amandasifa011@gmail.com>
2026-04-01 11:53:18 +0700

    lab 5

---
+++ javascript-basics/script.js
@@ -0,0 +1,43 @@
+var city = "Indramayu";
+let age = 20;
+const PI = 3.14;
+
+console.log(city);
+console.log(age);
+console.log(PI);
+
+let num = 42;
+let str = "Hello World";
+let bool = true;
+let arr = [1, 2, 3, 4, 5];
+let object = { name: "Amanda", age: 22, student: true };
+
+console.log(num);
+console.log(str);
+console.log(bool);
+console.log(arr);
+console.log(object);
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof arr);
+console.log(typeof object);
+
+let a = 10;
+let b = 3;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+console.log(a > b);
+console.log(a < b);
+console.log(a === 10);
+console.log(a !== b);
+
+console.log(a > 5 && b < 5);
+console.log(a > 20 || b < 5);
+console.log(!(a === b));
